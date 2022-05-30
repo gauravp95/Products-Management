be527156657b219a339abb6e720774aae144fbaf
gaurav95 <gauravpatil4195@gmail.com>
2022-05-30 16:14:47 +0530

    cartModel

---
+++ src/models/cartModel.js
@@ -0,0 +1,33 @@
+const mongoose =require('mongoose');
+const ObjectId = mongoose.Types.ObjectId
+
+const cartSchema = new mongoose.Schema({
+    userId: {
+        type:ObjectId, 
+        require: true,
+        unique: true,
+        ref: 'User'
+    },
+    items: {
+        productId : {
+            type: ObjectId,
+            require: true,
+            ref: 'Product'
+        },
+        quantity: {
+            type: Number,
+            require: true,
+            min: 1
+        }
+    },
+    totalPrice:{
+        type: Number,
+        require: true,
+    },
+    totalItems: {
+        type:Number,
+        require: true
+    }
+},{timestamps: true});
+
+module.exports = mongoose.model('Cart', cartSchema);
